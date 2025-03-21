66eb22758cd90026a114c8408b9a159abef1c8c2
zwelihle <zwelihle.zs@gmail.com>
2025-03-21 17:21:46 +0200

    UI/UX: completed the first screen(LandingPage) of 4 => Welcome Screen

--- Wazi/src/LandingPage.js
+++ Wazi/src/LandingPage.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { SafeAreaView,StyleSheet } from 'react-native';
+import { TextInput, Dimensions } from 'react-native';
+import { Button } from 'react-native';
+import { SafeAreaView,StyleSheet, Image, TouchableOpacity} from 'react-native';
 import { View, Text } from 'react-native';
+import logo from '../assets/wazi.png';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 export default function Landing() {
     return (
-        <SafeAreaView>
-            <View>
-                <Text style={styles.view}>Landing Page</Text>
-                <Text style={styles.text}>This page is loaded when app is run for the first time</Text>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.logoView}>
+                <Image style={styles.logo} source={logo}/>
             </View>
+            <View style={styles.input}>
+                <Text style={styles.titleText}>Please enter your City/Town</Text>
+                <TextInput style={styles.textInputComp}placeholder='Pretoria'></TextInput>
+            </View>
+            <TouchableOpacity style={styles.button} onPress={()=>{}}>
+                <Text style={styles.buttonText}>Get Weather</Text>
+            </TouchableOpacity>
             
         </SafeAreaView>
     );
@@ -26,12 +38,49 @@ const styles = StyleSheet.create({
         backgroundColor: 'purple',
         // height: 55,
         // width: 180,
-
+    },
+    logoView:{
+        marginTop: -(windowHeight * 25/100),
+    },
+    logo:{
+        marginTop:0,
+        width: 330,
+        height: 330
 
     },
+    textInputComp:{
+        backgroundColor: '#31e5d9',
+        fontSize:17,
+        borderRadius: 20,
+        borderColor: 'white',
+        borderWidth: 1,
+        padding:10,
+        width: 248,
+    },
+    titleText:{
+        fontSize:18,
+        alignSelf: 'center',
+        paddingBottom: 15
+    },
     text: {
-        color: 'grey',
+        fontSize:17,
+        color: '#363931',
         padding: 20,
+    },
+    button:{
+        backgroundColor: '#00bf',
+        margin: 20,
+        borderRadius: 25,
+        padding: 0.6
+
+    },
+    buttonText:{
+        backgroundColor: '#00b8f1',
+        borderRadius: 25,
+        borderColor: 'white',
+        borderWidth: 1.8,
+        paddingVertical:14,
+        paddingHorizontal: 30
     },
 
 
